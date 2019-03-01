035a27e6e5ce650d91cb30745620bc6fde93c948
Katie Schmidt <katie_schmidt7@icloud.com>
2019-03-01 07:56:30 -0500

    style the notecards

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,12 @@
+"use strict";
+
 const seeAnswerButton = document.querySelector(".button__see-answer");
+const seeQuestionButton = document.querySelector(".button__see-question");
 
 seeAnswerButton.onclick = () => {
-  console.log("see anser clicked");
+  console.log("see answer clicked");
+};
+
+seeQuestionButton.onclick = () => {
+  console.log("see question clicked");
 };
