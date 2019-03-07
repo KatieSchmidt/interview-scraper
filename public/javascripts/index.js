3551ceea64c587ac9e89eb2fa0dbb5a17578479c
Katie Schmidt <katie_schmidt7@icloud.com>
2019-03-07 15:03:28 -0500

    start working on theming

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -17,3 +17,15 @@ seeQuestionButton.onclick = () => {
   cardFront.classList.remove("display-none");
   cardBack.classList.add("display-none");
 };
+
+let colorArray = document.getElementsByClassName("color");
+// console.log(colorArray);
+
+for (var i = 0; i < colorArray.length; i++) {
+  colorArray[i].onclick = function(e) {
+    let targetClassString = e.target.classList.value;
+    let classList = targetClassString.split(" ");
+    let chosenColor = classList[1];
+    document.body.style.background = chosenColor;
+  };
+}
